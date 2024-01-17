e88e5961f903a0dbea7235879f8d9a0769b54493
Gracey <gracey@Graceys-MacBook-Air.local>
2024-01-17 09:54:42 -0800

    ChatGPT Test sample code

---
+++ foliager.py
@@ -0,0 +1,24 @@
+from openai import OpenAI
+
+client = OpenAI(api_key='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')
+
+import os
+
+import pandas as pd
+
+import time
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+
+    messages = [{"role": "user", "content": prompt}]
+
+    response = client.chat.completions.create(model=model, messages=messages, temperature=0)
+
+    return response.choices[0].message.content   
+
+prompt = "What kinds of trees can you find in the high desert?"
+
+response = get_completion(prompt)
+
+print(response)
