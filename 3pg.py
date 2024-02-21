f86bfa6cc361ea2be410ac288cdd82ed8afb36ce
Grace Todd <toddgr@oregonstate.edu>
2024-02-21 11:29:34 -0800

    init function

--- 3pg.py
+++ 3pg.py
@@ -6,3 +6,13 @@ Description: My attempt at converting 3-PG to Python so that I can use it in my 
              whatnot.
 """
 
+# So I'm going to start out by using Allison's implementation of a tree struct for the sake of visualization. 
+# In the future, though, I'll integrate this with my own tree_class. I jsut don't know how different they will
+# be yet, and whether or not it would be efficient to have them as the same class.
+
+class TreeViz:
+    def __init__(self, x, z, dbh_dev, height_dev, draw) -> None:
+        self.position = (x, z)          # X and Z coordinates of the spawn point (?) of the tree
+        self.dbh_dev = dbh_dev          # Diameter of breast height
+        self.height_dev = height_dev    # height of the tree
+        self.draw = draw                # Bool -- either draw or don't draw the tree
