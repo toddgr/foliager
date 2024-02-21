39027a0386baac0ac8f0638fded4849aa9db5654
Grace Todd <toddgr@oregonstate.edu>
2024-02-21 11:33:30 -0800

    init function, month class and treeviz  class

--- 3pg.py
+++ 3pg.py
@@ -11,8 +11,23 @@ Description: My attempt at converting 3-PG to Python so that I can use it in my 
 # be yet, and whether or not it would be efficient to have them as the same class.
 
 class TreeViz:
+    """
+        For visualizing the trees, originally in C++ code from Allison Thompson
+    """
     def __init__(self, x, z, dbh_dev, height_dev, draw) -> None:
         self.position = (x, z)          # X and Z coordinates of the spawn point (?) of the tree
         self.dbh_dev = dbh_dev          # Diameter of breast height
         self.height_dev = height_dev    # height of the tree
         self.draw = draw                # Bool -- either draw or don't draw the tree
+
+
+class Month:
+    """
+        Holds important information for visualization based on data from the site about the current month.
+    """
+    def __init__(self, site_tmax, site_tmin, site_rain, site_solar_rad, site_frost_days):
+        self.tmax = site_tmax   # Average maximum temperature for the month
+        self.tmin = site_tmin   # Average minimum temperature for the month
+        self.rain = site_rain   # Average rainfall for the month
+        self.solar_rad = site_solar_rad # Average solar radiation for the month
+        self.forst_days = site_frost_days   # Average number of frost days for the month
