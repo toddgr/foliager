b6e7537c05f470342ba5110851c0ded08c775ff9
Grace Todd <toddgr@oregonstate.edu>
2024-02-21 14:18:33 -0800

    create species data class

---
+++ 3pg_species_data.py
@@ -0,0 +1,15 @@
+"""
+File: 3pg.py
+Author: Grace Todd
+Date: February 21, 2024
+Description: Holds the SpeciesData class, which will be used to manipulate common parameters
+            for each of the species used in a simulation.
+"""
+
+class SpeciesData:
+    def __init__(self, ):
+        """
+            Reads in the CSV of the specific species data,
+            and parses it into the class object.
+            I don't think we really need anything more than that.
+        """
