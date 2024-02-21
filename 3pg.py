49ae9e54132d1c2646ef9ec91ed4e12b14682b13
Grace Todd <toddgr@oregonstate.edu>
2024-02-21 11:15:32 -0800

    Create 3PG file

---
+++ 3pg.py
@@ -0,0 +1,7 @@
+"""
+File: 3pg.py
+Author: Grace Todd
+Date: February 21, 2024
+Description: My attempt at converting 3-PG to Python so that I can use it in my calculations and 
+             whatnot.
+"""
