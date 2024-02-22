dde9cf50d0a27a5848161af2f08fcb25c36e6557
Grace Todd <toddgr@oregonstate.edu>
2024-02-22 10:24:10 -0800

    add option to plot, change y coords to z coords for 3D implementation

--- plot_trees_random.py
+++ plot_trees_random.py
@@ -1,34 +1,9 @@
-# import matplotlib.pyplot as plt
-# import numpy as np
-# from tree_class import TreeList
-# from parse_tree_input import parse_csv_file
-
-# def init_trees(foliage_file):
-#     # Generate random data
-#     np.random.seed(42)
-#     num_points = 100
-#     x_values = np.random.rand(num_points)
-#     y_values = np.random.rand(num_points)
-#     treelist = TreeList(parse_csv_file(foliage_file))  # Name, Growth Rate, Average Lifespan
-#     tree_names = treelist.get_tree_names()
-#     labels = np.random.choice(tree_names, num_points)  # Randomly select tree names
-
-#     # Define colors for each label
-#     label_colors = {label: plt.colormaps.get_cmap('viridis')(i / len(tree_names)) for i, label in enumerate(tree_names)}
-
-#     # Create a scatter plot with colored points
-#     for label, color in label_colors.items():
-#         plt.scatter(x_values[labels == label], y_values[labels == label], label=label, color=color)
-
-#     # Add labels and title
-#     plt.title('Initial Tree Placement')
-
-#     # Add legend
-#     plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
-#     plt.tight_layout()  # Adjust layout to prevent clipping
-
-#     # Show the plot
-#     plt.show()
+"""
+File name: plot_trees_random.py
+Author: Grace Todd
+Date: February 22, 2024
+Description: Initializes the coordinates for generated trees using scatter plot coordinates.
+"""
 
 import numpy as np
 import csv
