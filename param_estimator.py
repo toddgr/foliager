5e1dd28dc19d230fcc3d370420dd303a1c4b37b2
Grace Todd <toddgr@oregonstate.edu>
2024-04-29 08:52:02 -0700

    function definition

--- param_estimator.py
+++ param_estimator.py
@@ -5,3 +5,8 @@ Date: April 29, 2024
 Description: A parameter estimation prototype for generating realistic 
             tree data for trees that aren't in the knowledge base
 """
+
+knowledge_base = "test_data/species_data_kb.csv"
+
+def param_est(knowledge_base):
+    pass
