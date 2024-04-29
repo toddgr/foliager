34b2d0a6d16f4f63dee5091cb9282bd4579b0a4c
Grace Todd <toddgr@oregonstate.edu>
2024-04-29 08:49:47 -0700

    create the param estimator file

---
+++ param_estimator.py
@@ -0,0 +1,7 @@
+"""
+File: param_estimator.py
+Author: Grace Todd
+Date: April 29, 2024
+Description: A parameter estimation prototype for generating realistic 
+            tree data for trees that aren't in the knowledge base
+"""
