449eed9ca6a39c227ba1c1709f77be7a95c1f857
Grace Todd <56061990+toddgr@users.noreply.github.com>
2024-04-30 12:40:42 -0700

    blender file

---
+++ blender_place_trees.py
@@ -0,0 +1,8 @@
+"""
+File name: blender_place_trees.py
+Author: Grace Todd
+Date: April 30, 2024
+Description: This file aims to test out different scripts for the Blender API, so that I can hopefully 
+            Use Blender to generate the different tree assets and host the forest
+            Because I think that would be really cool
+"""
