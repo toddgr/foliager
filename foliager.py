c30c5b8c3c6f876ed7144c148002fc94a108dace
Grace Todd <toddgr@oregonstate.edu>
2024-08-16 13:45:36 -0700

    climate data generated and written to csv

--- foliager.py
+++ foliager.py
@@ -14,7 +14,8 @@ with open('parameters/secret_key.txt', 'r') as file:
     api_key = file.read()
 
 client = OpenAI(api_key=api_key)
-initial_attributes = "Common_name,scientific_name,leaf_shape_(oval/truncate/elliptical/lancolate/linear/other),canopy_density_(very_thin/thin/medium/dense/very_dense),deciduous_or_evergreen,leaf_color_(green),tree_form_(round/spreading/pyramidal/oval/conical/vase/columnar/open/weeping/irregular),tree_roots_(deep/shallow),habitat_(polar/temperate/dry/continental/tropical/subtropical/subcontinental/mediterranean/alpine/arid/subarctic/subalpine),bark_texture_(smooth/lenticels/furrows/ridges/cracks/scales/strips),bark_color_(gray/white/red/brown)\n"
+initial_species_attributes = "Common_name,scientific_name,leaf_shape_(oval/truncate/elliptical/lancolate/linear/other),canopy_density_(very_thin/thin/medium/dense/very_dense),deciduous_or_evergreen,leaf_color_(green),tree_form_(round/spreading/pyramidal/oval/conical/vase/columnar/open/weeping/irregular),tree_roots_(deep/shallow),habitat_(polar/temperate/dry/continental/tropical/subtropical/subcontinental/mediterranean/alpine/arid/subarctic/subalpine),bark_texture_(smooth/lenticels/furrows/ridges/cracks/scales/strips),bark_color_(gray/white/red/brown)\n"
+initial_climate_attributes = "month,tmax(average_maximum_monthly_temperature_celsius),tmin(average_minimum_monthly_temperature_celsius),rain(cm),solar_rad(kwh/m2),frost_days(average_number_of_monthly_frost_days),soil_texture(very_coarse_sand/coarse_sand/fine_sand/loamy_sand/sandy_loams/fine_sandy_loams/very_fine_sandy_loams/loams/silt_loams/clay_loams/silt_clay_loams/sandy_clay_loams/sandy_clays/silty_clays/clays)"
 
 import re
 from parse_tree_input import  csv_file_to_string
@@ -33,7 +34,7 @@ def ask_nlp(prompt, model="gpt-3.5-turbo"):
     return response.choices[0].message.content   
 
 def make_valid_filename(input_string):
-    extension = "_foliage.csv"
+    extension = ".csv"
     cleaned_string = re.sub(r'[^\w\s-]', '', input_string)  # Remove special characters except for spaces and hyphens
     cleaned_string = re.sub(r'\s+', '_', cleaned_string)    # Replace consecutive spaces with a single underscore
     cleaned_string = cleaned_string.strip('_-')
@@ -82,23 +83,35 @@ if __name__ == '__main__':
         species_prompt = generate_species_prompt(location)
         climate_prompt = generate_climate_prompt(location)
         
+        # get the species information
         species_response = ask_nlp(species_prompt) #comment out to save query time
         #print(response)
 
         # Write the foliage data to a csv file
-        nlp_response_filepath = "test_data/" + make_valid_filename(location)
-        with open(nlp_response_filepath, 'w') as file:
-            file.write(initial_attributes)
+        species_response_filepath = "test_data/" + make_valid_filename(location + " foliage")
+        with open(species_response_filepath, 'w') as file:
+            file.write(initial_species_attributes)
             file.write(species_response)
-            print("Writing to file ", nlp_response_filepath)
+            print(f"Writing to file {species_response_filepath}")
             # Now to parse input into Tree and TreeList objects
-        foliage_list = parse_csv_file(nlp_response_filepath)
+        foliage_list = parse_csv_file(species_response_filepath)
+
+        # get the climate information
+        climate_response = ask_nlp(climate_prompt)
+
+        # Write the climate information to a csv file
+        climate_response_filepath = "test_data/" + make_valid_filename(location + " climate")
+        with open(climate_response_filepath, 'w') as file:
+            file.write(initial_climate_attributes)
+            file.write(climate_response)
+
+            print(f"Writing to file {climate_response_filepath}")
 
     else:
         # use last NLP prompt (that I know works)
-        nlp_response_filepath = "test_data/portland_oregon_foliage.csv"
-        print(f"===== LLM NOT USED. =====\n Parsing tree data from {nlp_response_filepath}...")
-        foliage_list = parse_csv_file(nlp_response_filepath)
+        species_response_filepath = "test_data/portland_oregon_foliage.csv"
+        print(f"===== LLM NOT USED. =====\n Parsing tree data from {species_response_filepath}...")
+        foliage_list = parse_csv_file(species_response_filepath)
 
 
     print("\n===== ESTIMATING PARAMETERS for the following trees: ===== ")
