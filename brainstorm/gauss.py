dc307c6c8fa108d497d052a6ec58e163ff268ce6
Grace Todd <toddgr@oregonstate.edu>
2024-09-09 10:28:01 -0700

    Gaussian probability function file

---
+++ brainstorm/gauss.py
@@ -0,0 +1,7 @@
+"""
+File: gauss.py
+Author: Grace Todd
+Date: September 9, 2024
+Description: A Gaussian randomization function to create smoother looking averages. 
+            Adapted from C++ code provided by Mike Bailey.
+"""
