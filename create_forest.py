4f2718eed858ef8f287820d7a0ce5b44bfcb1ac2
Grace Todd <toddgr@oregonstate.edu>
2024-09-10 15:08:52 -0700

    block out forest class functions

--- create_forest.py
+++ create_forest.py
@@ -18,14 +18,80 @@ Description: Uses 3-PG to calculate various parameters of a tree, which will be 
              5. Take the final state of the forest and write to Blender
 """
 
+"""
+=====================================================================
+                                CLASSES
+=====================================================================
+"""
+
 class Forest:
     """
     Holds information about the environment, climate, and collection of trees found in the forest.
     Also might store a list of species found in the environment
     """
-    def __init__(self):
+    def __init__(self, climate_filepath):
+        """
+        Input: A list of climate conditions for each month of the year
+        Attributes:
+            - Climate : [ClimateByMonth]
+            - Trees : [Tree]
+        """
+        # create climate list
+        # read in climate_filepath
+            # for each row, create climatebymonth and append to climate list
+
+        # initialize list of trees
+        self.trees = []
+
         pass
 
+    def read_climate_data(self, climate_filepath):
+        """
+        Input: Climate CSV file
+        Output: A list of 12 ClimateByMonth instances, one for each month.
+
+        TODO I'm separating this out so that in the future I can easily change the
+        climate input -- CSV implementation is temporary
+        """
+        pass
+
+    def add_tree(self, tree):
+        self.trees.append(tree)
+
+    class ClimateByMonth:
+        """
+        A class that holds information on the climate for one month of the year.
+        A ClimateByMonth instance is initialized for each month of the year.
+        """
+        
+        def __init__(self, month:str, tmax:int, tmin:int, r:float, sr, fd, st):
+            """
+            Attributes:
+                - month : str
+                - tmax : int
+                - tmin : int
+                - rain : float
+                - solar_rad : float
+                - frost_days : int
+                - soil_texture : str
+                - soil_water : float        Estimated with estimate_soil_water
+                - max_soil_water : float
+            """
+            self.month = month          # January, February, etc.
+            self.tmax = tmax            # Average maximum temperature (celsius)
+            self.tmin = tmin            # Average minimum temperature (celsius)
+            self.rain = r               # Average rainfall (cm)
+            self.solar_rad = sr         # Average solar radiation (kwh/m2)
+            self.frost_days = fd        # Average number of frost days (int)
+            self.estimate_soil_water(st)
+        
+        def estimate_soil_water(self, soil_texture):
+            """
+            Uses soil texture input to estimate soil water
+
+            TODO use approximate_soil_data() here
+            """
+            pass
 
 class Species:
     """
@@ -46,6 +112,27 @@ class Tree(Species):
     def __init__(self, species, x, y):
         pass
 
+
+"""
+=====================================================================
+                                FUNCTIONS
+=====================================================================
+"""
+
+def create_forest():
+    # 1. Initialize forest
+    # read in the climate data
+    # initialize the forest based on climate data
+    
+    # 2. Compute data for each species
+
+    # 3. Create indivisual trees from species data
+
+    # 4. Repeat for spawned/killed trees
+
+    # 5. Write to Blender
+    pass
+
 if __name__ == '__main__':
     # example usage here
-    pass
+    create_forest()
