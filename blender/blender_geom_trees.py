9bb362aca83481b5ac2cf29bbeaef58b759b65b8
Grace Todd <56061990+toddgr@users.noreply.github.com>
2024-09-24 14:56:49 -0700

    initialize geometry node file:

---
+++ blender/blender_geom_trees.py
@@ -0,0 +1,13 @@
+"""
+File name: blender_place_trees.py
+Author: Grace Todd
+Date: September 24, 2024
+Description: This file uses Blender Geometry nodes and particle systems to generate tree
+            assets based on the input dimensions of a tree.
+"""
+
+import bpy
+import bmesh
+
+if __name__ == '__main__':
+    pass
