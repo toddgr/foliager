10bec1185add14035a1b44206f8806ecef525951
Grace Todd <56061990+toddgr@users.noreply.github.com>
2024-09-24 17:56:56 -0700

    functions to create and link nodes

--- blender/blender_geom_trees.py
+++ blender/blender_geom_trees.py
@@ -9,27 +9,39 @@ Description: This file uses Blender Geometry nodes and particle systems to gener
 import bpy
 #import bmesh
 
-def verify_units_metric():
-    # Get the current scene unit system
-    scene = bpy.context.scene
-    unit_settings = scene.unit_settings
+def create_node(node_tree, node_location, type_name):
+    node_obj = node_tree.nodes.new(type=type_name)
+    node_obj.location.x = node_location
+    node_location += 300
 
-    # Check if the unit system is set to 'METRIC'
-    if unit_settings.system != 'METRIC':
-        print("Warning: Unit system is not set to Metric. Setting it to Metric now.")
-        unit_settings.system = 'METRIC'
-    else:
-        print("Metric units are already in use.")
+    return node_obj, node_location
+
+def link_nodes(node_tree, from_node, from_type, to_node, to_type):
+    node_tree.links.new(from_node.outputs[from_type], to_node.inputs[to_type])
 
 def create_geometry_node_tree(obj):
     bpy.ops.node.new_geometry_nodes_modifier()
     node_tree = bpy.data.node_groups["Geometry Nodes"]
     node_tree.name = "ScriptTesting"
+
+    node_location = 0
+    in_node = node_tree.nodes["Group Input"]
+    in_node.location = (-300,0)
     
 
+    # add a mesh to curve node
+    mesh_to_curve, node_location = create_node(node_tree, node_location, "GeometryNodeMeshToCurve")
+    link_nodes(node_tree, in_node, "Geometry", mesh_to_curve, "Mesh")
+    
+    curve_to_mesh, node_location = create_node(node_tree, node_location, "GeometryNodeCurveToMesh")
+    link_nodes(node_tree, mesh_to_curve, "Curve", curve_to_mesh, "Curve")
+    
+    out_node = node_tree.nodes["Group Output"]
+    out_node.location = (node_location, 0)
+    link_nodes(node_tree, curve_to_mesh, "Mesh", out_node, "Geometry")
+
+
 def init_tree_mesh(name, height):
-    # Make sure that everything is in meters
-    verify_units_metric()
 
     # Define the vertices
     vertices = [(0, 0, 0), (0, 0, height)]
