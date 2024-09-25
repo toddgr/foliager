541fdd489d53d5b14e50836141e968bf803fb775
Grace Todd <56061990+toddgr@users.noreply.github.com>
2024-09-24 20:53:32 -0700

    add dbh integration, wrap in frames:

--- blender/blender_geom_trees.py
+++ blender/blender_geom_trees.py
@@ -14,10 +14,12 @@ import bpy
 
 DBH = 0.203 # diameter at breast height for now
 
+SPACING = 250
+
 def create_node(node_tree, node_location, type_name):
     node_obj = node_tree.nodes.new(type=type_name)
     node_obj.location.x = node_location
-    node_location += 300
+    node_location += SPACING
 
     return node_obj, node_location
 
@@ -47,38 +49,53 @@ def create_tree_base(node_tree):
     node_x_location = 0
     node_y_location = 0
 
+
     in_node = node_tree.nodes["Group Input"]
-    in_node.location = (-300,0)
+    in_node.location = (-SPACING,0)
+    
+    # Create a Frame node
+    frame_node = node_tree.nodes.new(type='NodeFrame')
+    frame_node.label = "Create the tree trunk/base"
+    frame_node.location = (node_x_location, node_y_location)  # Position the frame
 
     # mesh to curve
     mesh_to_curve, node_x_location = create_node(node_tree, node_x_location, "GeometryNodeMeshToCurve")
     link_nodes(node_tree, in_node, "Geometry", mesh_to_curve, "Mesh")
+    mesh_to_curve.parent = frame_node
 
     # trim curve
     trim_curve, node_x_location = create_node(node_tree, node_x_location, "GeometryNodeTrimCurve")
     link_curve_nodes(node_tree, mesh_to_curve, trim_curve)
+    trim_curve.parent = frame_node
 
     # resample curve
     resample_curve, node_x_location = create_node(node_tree, node_x_location, "GeometryNodeResampleCurve")
     link_curve_nodes(node_tree, trim_curve, resample_curve)
+    resample_curve.parent = frame_node
 
     # set curve radius
-    curve_circle_location = (node_x_location, node_y_location - 150)
+    curve_circle_location = (node_x_location, node_y_location - (SPACING / 2))
     set_curve_radius, node_x_location = create_node(node_tree, node_x_location, "GeometryNodeSetCurveRadius")
     link_curve_nodes(node_tree, resample_curve, set_curve_radius)
+    set_curve_radius.parent = frame_node
 
     #   set trunk thickness
-    curve_radius = set_trunk_thickness(node_tree, DBH, node_x_location-(300 * 3), node_y_location + 300)
+    curve_radius = set_trunk_thickness(node_tree, DBH, node_x_location-(SPACING * 5), node_y_location + SPACING)
     link_nodes(node_tree, curve_radius, "Value", set_curve_radius, "Radius")
 
     # curve to mesh
     curve_to_mesh, node_x_location = create_node(node_tree, node_x_location, "GeometryNodeCurveToMesh")
     link_curve_nodes(node_tree, set_curve_radius, curve_to_mesh)
+    curve_to_mesh.parent = frame_node
 
     #   curve circle
     curve_circle, node_x_location = create_node(node_tree, node_x_location, "GeometryNodeCurvePrimitiveCircle")
     curve_circle.location = curve_circle_location
     link_nodes(node_tree, curve_circle, "Curve", curve_to_mesh, "Profile Curve")
+    curve_circle.parent = frame_node
+    
+    frame_node.width = (curve_to_mesh.location.x  - mesh_to_curve.location.x)  # Adjust width to fit nodes
+    frame_node.height = (curve_circle.location.y  - mesh_to_curve.location.y) # Adjust height
     
     return node_x_location, curve_to_mesh
 
@@ -87,29 +104,35 @@ def set_trunk_thickness(node_tree, dbh, node_x_location, node_y_location):
     Input: Diameter at breast height (m)
     Output: Radius value for Set Curve Radius
     """
-    # create the group
+    # Create a Frame node
+    frame_node = node_tree.nodes.new(type='NodeFrame')
+    frame_node.label = "Taper trunk / Scale to DBH"
+    frame_node.location = (node_x_location, node_y_location)  # Position the frame
 
     # spline parameter
     spline_parameter, node_x_location = create_node(node_tree, node_x_location, "GeometryNodeSplineParameter")
     spline_parameter.location.y = node_y_location
+    spline_parameter.parent = frame_node
 
     # float curve
     float_curve, node_x_location = create_node(node_tree, node_x_location, "ShaderNodeFloatCurve")
-    float_curve.location.y = node_y_location + 100
+    float_curve.location.y = node_y_location + (SPACING / 2)
     link_nodes(node_tree, spline_parameter, "Factor", float_curve, "Value")
+    float_curve.parent = frame_node
 
     # subtract from 1
     subtract_location = (node_x_location, node_y_location)
     subtract, node_x_location = create_node(node_tree, node_x_location, "ShaderNodeMath")
-    #subtract = node_tree.nodes.new(type="ShaderNodeMath")
     subtract.operation = 'SUBTRACT'
     subtract.inputs[1].default_value = 1.0  # Set the second input to 1
     subtract.location = subtract_location
     link_nodes(node_tree, float_curve, "Value", subtract, "Value")
+    subtract.parent = frame_node
 
     # multiply
     multiply, node_x_location = create_node(node_tree, node_x_location, "ShaderNodeMath")
     multiply.operation = 'MULTIPLY'
+    multiply.inputs[1].default_value = dbh
     multiply.location.y = node_y_location
     link_nodes(node_tree, subtract, "Value", multiply, "Value")
 
@@ -117,7 +140,7 @@ def set_trunk_thickness(node_tree, dbh, node_x_location, node_y_location):
 
     # group output
     
-    return subtract # a group
+    return multiply # a group
 
 
 def make_tree_base():
